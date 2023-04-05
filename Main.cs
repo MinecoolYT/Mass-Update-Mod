3865b1176615731499666f189099465919e56372
MinecoolYT <72187396+MinecoolYT@users.noreply.github.com>
2023-04-05 05:28:32 -0500

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,57 @@
+﻿using HarmonyLib;
+using Il2CppAssets.Scripts.Simulation.Towers;
+using Il2CppAssets.Scripts.Unity.Towers.Upgrades;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame.TowerSelectionMenu;
+using MelonLoader;
+using System;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(MassUpdate.Main), "Mass Update", "1.0.0", "Minecool")]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+namespace MassUpdate
+{
+    public class Main : MelonMod
+    {
+        [HarmonyPatch(typeof(Tower), nameof(Tower.OnSold))]
+        public class SellPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Tower __instance)
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    foreach (var tower in InGame.instance.bridge.GetAllTowers())
+                    {
+                        if (tower.Def.name == __instance.model.name)
+                        {
+                            InGame.instance.SellTower(tower);
+                        }
+                    }
+                }
+            }
+        }
+        static bool allowUpgrade = true;
+        [HarmonyPatch(typeof(Il2CppAssets.Scripts.Unity.Bridge.TowerToSimulation), nameof(Upgrade))]
+        public class TestPatch
+        {
+            [HarmonyPostfix]
+            public static void Postfix(int pathIndex, bool isParagon, Action<bool> callback)
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    if (!allowUpgrade) return;
+                    allowUpgrade = false;
+                    foreach (var tower in InGame.instance.bridge.GetAllTowers())
+                    {
+                        if (tower.Def.name == TowerSelectionMenu.instance.selectedTower.tower.model.name && tower.id != TowerSelectionMenu.instance.selectedTower.id)
+                        {
+                            tower.Upgrade(pathIndex, isParagon, callback);
+                        }
+                    }
+                    allowUpgrade = true;
+                }
+            }
+        }
+    }
+}
